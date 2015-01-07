c89d21c1586c4476953b3525d904ae052a83cba5
pirhoo <pierre.romera@gmail.com>
2015-01-07 12:38:20 +0100

    Added locale to watch task

--- gulp/watch.js
+++ gulp/watch.js
@@ -2,10 +2,11 @@
 
 var gulp = require('gulp');
 
-gulp.task('watch', ['consolidate', 'wiredep', 'injector:css', 'injector:js'] ,function () {
+gulp.task('watch', ['consolidate', 'wiredep', 'injector:css', 'injector:js', 'locale'] ,function () {
   gulp.watch('src/{app,components}/**/*.less', ['injector:css']);
   gulp.watch('src/{app,components}/**/*.{js,coffee}', ['injector:js']);
   gulp.watch('src/assets/images/**/*', ['images']);
   gulp.watch('bower.json', ['wiredep']);
   gulp.watch('src/{app,components}/**/*.jade', ['consolidate:jade']);
+  gulp.watch('locale//**/*.json', ['locale']);
 });
